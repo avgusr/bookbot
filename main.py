091b7ee13ec5a2c1df6767f68a33149943f13dff
oscars <oscar.avitia@protonmail.com>
2025-03-28 22:56:09 -0700

    final report fucntion added to main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from stats import get_num_words, get_char_count
+from stats import get_num_words, get_char_count, book_report
 def get_book_text(filepath: str) -> str:
     with open(filepath) as f:
         file_content = f.read()
@@ -9,8 +9,9 @@ def get_book_text(filepath: str) -> str:
     
 def main():
     FILEPATH:str = "./books/frankenstein.txt"
-    print(get_num_words(get_book_text(FILEPATH)))
-    print(get_char_count(get_book_text(FILEPATH)))
+    word_num = get_num_words(get_book_text(FILEPATH))
+    char_counter:dict = get_char_count(get_book_text(FILEPATH))
+    book_report(char_counter,word_num,FILEPATH)
     
 
 main()
