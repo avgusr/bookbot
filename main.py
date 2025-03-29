b7b6af9666fccf86f62263e04e1a0ed2aac09e86
oscars <oscar.avitia@protonmail.com>
2025-03-29 00:03:39 -0700

    Added sys args

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 
 from stats import get_num_words, get_char_count, book_report
+import sys
+
+if len(sys.argv) == 1:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 def get_book_text(filepath: str) -> str:
     with open(filepath) as f:
         file_content = f.read()
@@ -8,7 +14,7 @@ def get_book_text(filepath: str) -> str:
     
     
 def main():
-    FILEPATH:str = "./books/frankenstein.txt"
+    FILEPATH:str = 'books/frankenstein.txt'
     word_num = get_num_words(get_book_text(FILEPATH))
     char_counter:dict = get_char_count(get_book_text(FILEPATH))
     book_report(char_counter,word_num,FILEPATH)
