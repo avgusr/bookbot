4f2df8c16c90a0787613e7a3cd748c71114b6693
oscars <oscar.avitia@protonmail.com>
2025-03-28 20:06:12 -0700

    Added get_char_count func

--- stats.py
+++ stats.py
@@ -1,3 +1,17 @@
-def get_num_words(file_content) -> int:
+def get_num_words(file_content:str) -> int:
     num_words = len(file_content.split())
-    print(f"{num_words} words found in the document")
+    return f"{num_words} words found in the document"
+
+
+
+def get_char_count(file_content:str) -> dict:
+
+    char_counter:dict[str:int] = {}
+
+    for char in file_content.lower():
+        if char not in char_counter:
+            char_counter[char] = 1
+        else:
+            char_counter[char] += 1
+    
+    return char_counter
