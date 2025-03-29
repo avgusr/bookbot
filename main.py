49e30d872e97b2135b91d513e6f3a7cb4ae979cf
oscars <oscar.avitia@protonmail.com>
2025-03-28 20:05:37 -0700

    Added character function

---
+++ main.py
@@ -0,0 +1,18 @@
+
+from stats import get_num_words, get_char_count
+def get_book_text(filepath: str) -> str:
+    with open(filepath) as f:
+        file_content = f.read()
+    
+    return file_content
+    
+    
+def main():
+    FILEPATH : str = "./books/frankenstein.txt"
+    # # print(get_book_text("./books/frankenstein.txt"))
+    # book_text = get_book_text(FILEPATH)
+    print(get_num_words(get_book_text(FILEPATH)))
+    print(get_char_count(get_book_text(FILEPATH)))
+    
+
+main()
